e42a5b3f10e033331e39c243bde533c15e9be9b8
Harish Iyer (harishGitLearn) <imharish@gmail.com>
2017-03-21 10:44:44 +0530

    Initial Commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author harishi
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+	}
+
+}
