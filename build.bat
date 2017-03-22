b0a86c681c650dcee200a800f557142f4121fa33
Harish Iyer (harishGitLearn) <imharish@gmail.com>
2017-03-22 11:09:09 +0530

    removed reference to enc vars

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 PATH = %PATH%;C:\Program Files\Java\jdk1.8.0_121\bin\
 
-javac -classpath "%CLASSPATH%;%JUNIT_HOME%\junit-4.12.jar;%JUNIT_HOME%\hamcrest-core-1.3.jar;%ANT_HOME%\lib\;C:\Program Files\Java\jre1.8.0_121\lib\;.;" *.java
+javac -classpath "C:\D Drive\GoCD\junit-4.12.jar;C:\D Drive\GoCD\hamcrest-core-1.3.jar;C:\D Drive\GoCD\apache-ant-1.10.1\lib\;C:\Program Files\Java\jre1.8.0_121\lib\;.;;C:\D Drive\GoCD\junit-4.12.jar;C:\D Drive\GoCD\hamcrest-core-1.3.jar;C:\D Drive\GoCD\apache-ant-1.10.1\lib\;C:\Program Files\Java\jre1.8.0_121\lib\;.;" *.java
