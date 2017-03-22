7bde0bb2a40fb22d7b8823da60f23d53ffbfdfc0
Harish Iyer (harishGitLearn) <imharish@gmail.com>
2017-03-22 11:16:05 +0530

    Adding batchfile to run the test

---
+++ runtest.bat
@@ -0,0 +1,1 @@
+java -classpath "C:\D Drive\GoCD\junit-4.12.jar;C:\D Drive\GoCD\hamcrest-core-1.3.jar;C:\D Drive\GoCD\apache-ant-1.10.1\lib\;C:\Program Files\Java\jre1.8.0_121\lib\;.;;C:\D Drive\GoCD\junit-4.12.jar;C:\D Drive\GoCD\hamcrest-core-1.3.jar;C:\D Drive\GoCD\apache-ant-1.10.1\lib\;C:\Program Files\Java\jre1.8.0_121\lib\;.;" TestRunner
