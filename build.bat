15e2c20a3ab22a5ce191ede49145e5e3ec3e8c54
Harish Iyer (harishGitLearn) <imharish@gmail.com>
2017-03-22 11:02:51 +0530

    modified build batch hardcoded classpath

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 PATH = %PATH%;C:\Program Files\Java\jdk1.8.0_121\bin\
 
-javac -classpath "%CLASSPATH%" *.java
+javac -classpath "%CLASSPATH%;%JUNIT_HOME%\junit-4.12.jar;%JUNIT_HOME%\hamcrest-core-1.3.jar;%ANT_HOME%\lib\;C:\Program Files\Java\jre1.8.0_121\lib\;.;" *.java
